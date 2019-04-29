875a724853cba57d78533f4b3227c0ca4075b9e4
luisgustavogomes <luisgustavogomes@outlook.com>
2019-04-28 23:35:30 -0300

    Criando arquitetura Owin

---
+++ ByteBank.ForumNaviteFramework/Controllers/ContaController.cs
@@ -0,0 +1,61 @@
+﻿using ByteBank.ForumNaviteFramework.Models;
+using ByteBank.ForumNaviteFramework.Models.ViewModels;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ByteBank.ForumNaviteFramework.Controllers
+{
+    public class ContaController : Controller
+    {
+
+        private UserManager<UsuarioAplicacao> _userManager;
+        public UserManager<UsuarioAplicacao> UserManager
+        {
+            get
+            {
+                if (_userManager == null)
+                {
+                    _userManager = HttpContext.GetOwinContext().GetUserManager<UserManager<UsuarioAplicacao>>();
+                }
+                return _userManager;
+            }
+            set
+            {
+                _userManager = value;
+            }
+        }
+
+
+        public ActionResult Registrar()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult> Registrar(ContaRegistrarViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                
+                var novoUsuario = new UsuarioAplicacao
+                {
+                    Email = model.Email,
+                    UserName = model.UserName,
+                    NomeCompleto = model.NomeCompleto
+                };
+
+                await UserManager.CreateAsync(novoUsuario, model.Senha);
+
+                return RedirectToAction("Index", "Home");
+            }
+            return View(model);
+        }
+
+
+    }
+}
---
+++ ByteBank.ForumNaviteFramework/Startup.cs
@@ -0,0 +1,33 @@
+﻿using ByteBank.ForumNaviteFramework.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using Owin;
+using System.Data.Entity;
+
+[assembly: OwinStartup(typeof(ByteBank.ForumNaviteFramework.Startup))]
+namespace ByteBank.ForumNaviteFramework
+{
+    public class Startup
+    {
+
+        public void Configuration(IAppBuilder builder)
+        {
+            builder.CreatePerOwinContext<DbContext>(() => new IdentityDbContext<UsuarioAplicacao>("DefaultConnection"));
+            builder.CreatePerOwinContext<IUserStore<UsuarioAplicacao>>(
+                (opcoes, contextoOwin) =>
+                {
+                    var dbContext = contextoOwin.Get<DbContext>();
+                    return new UserStore<UsuarioAplicacao>(dbContext);
+                });
+
+            builder.CreatePerOwinContext<UserManager<UsuarioAplicacao>>(
+                (opcoes, contextoOwin) =>
+                {
+                    var userStore = contextoOwin.Get<IUserStore<UsuarioAplicacao>>();
+                    return new UserManager<UsuarioAplicacao>(userStore);
+                });
+        }
+    }
+}
